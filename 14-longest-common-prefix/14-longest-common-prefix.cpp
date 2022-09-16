16088914e43a3c46ef368ad2cf3ac9b0c0d3b3ba
kriti-002 <66835642+kriti-002@users.noreply.github.com>
2022-09-16 22:59:40 +0530

    Time: 8 ms (47.73%), Space: 10.1 MB (9.88%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    struct Trie{
+        unordered_map<char,Trie*> child;
+        bool isword=false;
+    }root;
+    void insert(string word){
+        auto cur=&root;
+        for(char c : word){
+            if(cur->child.find(c)==cur->child.end())
+                cur->child[c]=new Trie();
+            cur=cur->child[c];
+        }
+        cur->isword=true;
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==0) return "";
+        for(auto word:strs)
+            insert(word);
+        
+        auto cur=&root;
+        string res="";
+        while(cur&&!cur->isword&&(cur->child.size()==1)){
+            auto it=cur->child.begin();
+            res+=it->first;
+            cur=it->second;
+        }
+        return res;
+    }
+};
