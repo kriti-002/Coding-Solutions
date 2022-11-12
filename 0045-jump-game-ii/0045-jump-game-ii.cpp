fa3f9c58252af1667b5718e7313274af881aa5af
kriti-002 <66835642+kriti-002@users.noreply.github.com>
2022-11-12 22:07:14 +0530

    Time: 1001 ms (13.05%), Space: 18 MB (12.93%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int help(int i, int n, vector<int>&nums, vector<int>&dp){
+        if(i >=n-1) return 0;
+        if(dp[i]!=-1) return dp[i];
+        
+        int maxi=n, check=0;
+        for(int j=i+1; j<=i+nums[i]; j++){
+            maxi=min(maxi, 1+ help(j, n, nums, dp));
+        }
+        return dp[i]=maxi;
+    }
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n, -1);
+        return help(0,n,nums, dp);
+    }
+};
