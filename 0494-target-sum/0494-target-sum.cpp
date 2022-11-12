ea896dbb859071020b18575e026fc8c3525a91ba
kriti-002 <66835642+kriti-002@users.noreply.github.com>
2022-11-12 11:11:40 +0530

    Time: 25 ms (68.58%), Space: 12 MB (48.77%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int help(vector<int>&arr, vector<vector<int>>&dp, int idx, int target){
+    if(idx==0){
+        if(target==0 and arr[idx]==0) return 2;
+        if(target==0 or arr[idx]==target) return 1;
+        return 0;
+    }
+    if(dp[idx][target]!=-1) return dp[idx][target];
+    
+    int pick=0;
+    int notPick= help(arr,dp, idx-1, target);
+    if(arr[idx]<= target) pick= help(arr, dp, idx-1, target-arr[idx]);
+    return dp[idx][target]=(pick+ notPick);
+}
+    
+int countPartitions(int n, int d, vector<int> &arr) {
+    // Write your code here.
+    int sum=0;
+    for(auto &x: arr) sum+=x;
+    if((sum-d)<0 or (sum-d)&1) return false;
+    int target=(sum-d)/2;
+    vector<vector<int>>dp(n, vector<int>(target+1,-1));
+    return help(arr, dp, n-1, target);
+}
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return countPartitions(nums.size(), target, nums);
+    }
+};
