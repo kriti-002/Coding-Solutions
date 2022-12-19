eb29ef9bf01caf3a6454e9df9cce054b0da8937d
kriti-002 <66835642+kriti-002@users.noreply.github.com>
2022-12-19 13:09:22 +0530

    Time: 1137 ms (8.33%), Space: 231.2 MB (13.91%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -7,12 +7,11 @@ public:
         int dp[n+1][m+1];
         int res=0; string result="";
         
-        for(int i=0; i<n+1; i++) dp[i][0]=0;
-        for(int j=0; j<=m; j++) dp[0][j]=0;
         
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(s[i-1]==t[j-1]){
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i==0 or j==0) dp[i][j]=0;
+                else if(s[i-1]==t[j-1]){
                     dp[i][j]= 1+ dp[i-1][j-1];
                     if(dp[i][j]> res){
                         string temp = s.substr(i-dp[i][j], dp[i][j]) ;
@@ -26,10 +25,6 @@ public:
                 }else dp[i][j]=0;
             }
         }
-        
-        return result;
-        
-        
-        
+        return result;     
     }
 };
