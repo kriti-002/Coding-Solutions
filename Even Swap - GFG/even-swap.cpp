42828a6599f2a819c3d5e53a421e05423dad7206
kriti-002 <66835642+kriti-002@users.noreply.github.com>
2023-03-31 19:14:17 +0530

    Added solution - LeetHub

---
+++ Even Swap - GFG/even-swap.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+        vector <int> lexicographicallyLargest(vector <int> &a,int n)
+        {
+            // Code here
+            int store=0;
+            for(int i=1; i<n; i++){
+                int tmp= a[i]+a[i-1];
+                if(tmp&1){
+                    sort(a.begin()+store, a.begin()+i, greater<int>());
+                    store=i;
+                }
+            }
+            sort(a.begin()+store, a.end(), greater<int>());
+            return a;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector <int> a(n);
+        for(auto &j:a)
+            cin>>j;
+        Solution s;
+        vector <int> b=s.lexicographicallyLargest(a,n);
+        for(auto i:b)
+            cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
