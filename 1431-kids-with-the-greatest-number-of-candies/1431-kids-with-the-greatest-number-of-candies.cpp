65738f7f09bc13a6bf048d1ccba5d1b7e22cef0c
kriti-002 <66835642+kriti-002@users.noreply.github.com>
2023-04-17 10:05:33 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (98.95%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -3,8 +3,8 @@ public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
         vector<bool>res;
         int maxi= *max_element(candies.begin(), candies.end());
-        for(int i=0; i<candies.size(); i++){
-            if(candies[i] + extraCandies >= maxi) res.push_back(1);
+        for(auto c: candies){
+            if(c + extraCandies >= maxi) res.push_back(1);
             else res.push_back(0);
         }
         
