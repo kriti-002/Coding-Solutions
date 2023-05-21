4850cba9119452a091239f0304906bb5f55deb9c
kriti-002 <66835642+kriti-002@users.noreply.github.com>
2023-05-21 12:02:58 +0530

    Added solution - LeetHub

---
+++ Bus Conductor - GFG/bus-conductor.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int findMoves(int n, vector<int> chairs, vector<int> passengers) {
+        sort(chairs.begin(),chairs.end());
+        sort(passengers.begin(),passengers.end());
+        int sum=0;
+        int res;
+        for(int i=0;i<n;i++){
+            res=0;
+            res=abs(passengers[i]-chairs[i]);
+            sum+=res;
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tt;
+    cin >> tt;
+    Solution sol;
+    while (tt--) {
+        int n;
+        cin >> n;
+        vector<int> chairs(n), passengers(n);
+        for (auto &val : chairs) cin >> val;
+        for (auto &val : passengers) cin >> val;
+        int ans = sol.findMoves(n, chairs, passengers);
+        cout << ans << '\n';
+    }
+    return 0;
+}
+// } Driver Code Ends
