f54a6a1678cc7b4f63686eb7d577ad5103746918
Kriti Srivastava <66835642+kriti-002@users.noreply.github.com>
2023-07-08 12:36:03 +0530

    Time: 6 ms (29.32%), Space: 7.4 MB (99.42%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -12,21 +12,9 @@ class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
         if(!head or !head->next) return head;
-        ListNode* prev= NULL;
-        ListNode* slow=head; ListNode* fast= head->next;
-        head=head->next;
-        while(slow and fast){
-            if(prev){
-                prev->next=fast;
-            }
-            slow->next = fast->next;
-            fast->next = slow;
-            prev = slow;
-            slow=slow->next;
-            if (slow)
-                fast = slow->next;
-        }
-    
-        return head;
+        ListNode* tmp=head->next;
+        head->next=swapPairs(head->next->next);
+        tmp->next=head;
+        return tmp;
     }
 };
