5ac096f3a021ce625fb666db6e20ee58956ae44a
Kriti Srivastava <66835642+kriti-002@users.noreply.github.com>
2023-07-16 08:11:12 +0530

    Added solution - LeetHub

---
+++ Trailing zeroes in factorial - GFG/trailing-zeroes-in-factorial.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    int trailingZeroes(int n)
+    {
+        // Write Your Code here
+        int result = 0;
+        for(long long i=5; n/i>0; i*=5){
+            result += (n/i);
+        }
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        int ans  = ob.trailingZeroes(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
