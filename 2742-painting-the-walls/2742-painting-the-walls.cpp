b0898d64e082c0829325241a9ea9a50285cd33da
Kriti Srivastava <66835642+kriti-002@users.noreply.github.com>
2023-10-14 08:00:02 +0530

    Time: 165 ms (49.77%), Space: 115.7 MB (45.71%) - LeetHub

---
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    int help(int i, int wall, vector<int>&cost, vector<int>&time, vector<vector<int>>&dp){
+        int n=cost.size();
+        if(wall<=0) return 0;
+        if(i==n) return 1e9;
+        if(dp[i][wall]!=-1) return dp[i][wall];
+        
+        int paid= cost[i] + help(i+1, wall-1-time[i], cost, time, dp), unpaid= help(i+1, wall, cost, time, dp);
+        return dp[i][wall]= min(paid, unpaid);
+    }
+public:
+    int paintWalls(vector<int>& cost, vector<int>& time) {
+        int n=cost.size();
+        vector<vector<int>>dp(n, vector<int>(n+1, -1));
+        return help(0,n, cost, time, dp);
+    }
+};
