9cd67b13112a70c291c5707c85b057884ffa564b
Kriti Srivastava <66835642+kriti-002@users.noreply.github.com>
2023-10-23 16:05:51 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (13.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n<=0) return false;
-        int x=log2(n)/log2(4);
+        int x=log2(n)/2;
         if(pow(4,x)==n) return true;
         return false;
     }
