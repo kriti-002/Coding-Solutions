f58f72ff0ab3bae6ec6c5d64f1f6c160c47ce361
Kriti Srivastava <66835642+kriti-002@users.noreply.github.com>
2023-10-26 08:22:55 +0530

    Time: 55 ms (46.06%), Space: 10.6 MB (31.10%) - LeetHub

---
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    int mod = 1e9 + 7;
+    unordered_map<int, long long int> dp;
+    unordered_set<int> st;
+private:
+    long long int factorCount(int root)
+    {
+        if(dp.count(root)) return dp[root];
+    
+        long long int count = 1;
+ 
+        for(auto &val : st){
+            // check if root is divisible or not
+            if(root % val != 0) continue;
+            
+            int factor1 = val, factor2 = root / val;
+            
+            // check its factor is present in given arr
+            if(st.count(factor2)){
+                count += factorCount(factor1) * factorCount(factor2);
+                count %= mod;
+            }
+            
+         }
+        
+        return dp[root] = count;
+    }
+    
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+                
+        st.insert(arr.begin(), arr.end());
+        
+        int res = 0;
+        
+        for(auto &curVal : arr){
+            res += factorCount(curVal);
+            res %= mod;
+        }
+        
+        return res;
+    }
+};
